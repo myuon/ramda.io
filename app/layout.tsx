ad9ed0b1caa6fff7c51fa11e60e262379fb1beb0
myuon <ioi.joi.koi.loi@gmail.com>
2024-02-12 18:02:42 +0900

    chore: add @vercel/analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { KumaRegistry } from "@kuma-ui/next-plugin/registry";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         <KumaRegistry>{children}</KumaRegistry>
+        <Analytics />
       </body>
     </html>
   );
